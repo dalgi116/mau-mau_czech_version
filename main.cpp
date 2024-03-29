96cd1ecc5d38729807190ab65536696069985257
Daniel Franc <daniel.franc@email.cz>
2024-03-29 15:35:03 +0100

    game preparation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,59 @@
 #include <iostream>
+#include "main.hpp"
 
 int main()
 {
-    std::cout << "Hello world!\n";
+    std::vector<Card> allCards = {
+        {"Under", 'H'},
+        {"Ober", 'H'},
+        {"King", 'H'},
+        {"Ace", 'H'},
+        {"VII", 'H'},
+        {"VIII", 'H'},
+        {"IX", 'H'},
+        {"X", 'H'},
+        
+        {"Under", 'B'},
+        {"Ober", 'B'},
+        {"King", 'B'},
+        {"Ace", 'B'},
+        {"VII", 'B'},
+        {"VIII", 'B'},
+        {"IX", 'B'},
+        {"X", 'B'},
+
+        {"Under", 'A'},
+        {"Ober", 'A'},
+        {"King", 'A'},
+        {"Ace", 'A'},
+        {"VII", 'A'},
+        {"VIII", 'A'},
+        {"IX", 'A'},
+        {"X", 'A'},
+
+        {"Under", 'L'},
+        {"Ober", 'L'},
+        {"King", 'L'},
+        {"Ace", 'L'},
+        {"VII", 'L'},
+        {"VIII", 'L'},
+        {"IX", 'L'},
+        {"X", 'L'},
+    };
+
+    DrawPile drawPile(32, allCards);
+    DiscardPile discardPile;
+    Hand playersHand, botsHand;
+
+
+
+    std::cout << "Hello, let's play!\n";
+
+    // prepare a game
+    drawPile.shuffleCards();
+    drawPile.moveCards(4, playersHand);
+    drawPile.moveCards(4, botsHand);
+    drawPile.moveCards(1, discardPile);
+
     return 0;
 }
--- main.hpp
+++ main.hpp
@@ -1,11 +1,19 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <random>
+#include <stdexcept>
+#include <iostream>
 
 class Card
 {
 public:
     std::string type;
     char color;
+    Card (std::string t, char c) : type(t), color(c) {};
 };
 
 class Pile
@@ -13,21 +21,52 @@ class Pile
 public:
     std::vector<Card> cards;
     int count;
-    void moveCards(int moveCount, Pile moveTo) {};
+    void moveCards(int moveCount, Pile& moveTo)
+    {
+        if (moveCount > this->count)
+            throw std::invalid_argument("Not enough cards.\n");
+        
+        for (int i = 0; i < moveCount; i++)
+        {
+            moveTo.cards.push_back(this->cards.back());
+            this->cards.pop_back();
+            moveTo.count ++;
+            this->count --;
+        }
+    }
+    void printInfo()
+    {
+        std::cout << "Count: " << this->count << "\n";
+        for (auto a : this->cards)
+            std::cout << "Type: " << a.type << ", Color: " << a.color << "\n";
+    }
+    Pile() : count(0) {};
+    Pile(int a, std::vector<Card> b) : count(a), cards(b) {};
 };
 
 class DrawPile : public Pile
 {
-    void shuffleCards() {};
+public:
+    void shuffleCards()
+    {
+        std::random_device rd;
+        std::default_random_engine rng(rd());
+        std::shuffle(this->cards.begin(), this->cards.end(), rng);
+    }
+    DrawPile(int a, std::vector<Card> b) : Pile(a, b) {};
 };
 
 class DiscardPile : public Pile
 {
+public:
     void printLastCard() {};
 };
 
 class Hand : public Pile
 {
+public:
     void drawCards(int drawCount) {};
     void printCards() {};
 };
+
+#endif /* MAIN_HPP */
